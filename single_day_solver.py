515ab583de2c9cad0bec3d72cd501907956879f0
Josh Smith <jbsmith@hamilton.edu>
2025-03-29 20:47:04 -0400

    Finish SingleDaySolver class and fix bugs

--- single_day_solver.py
+++ single_day_solver.py
@@ -1,6 +1,7 @@
 from ortools.linear_solver import pywraplp
 from typing import List
 from swimmer import Swimmer
+import re
 
 ROSTER_SIZE = 8
 BUDGET = 200
@@ -19,6 +20,7 @@ class SingleDaySolver:
 
         self.num_females = len(self.female_swimmers)
         self.num_males = len(self.male_swimmers)
+        self.solution_values = []
     
 
     def get_data(self):
@@ -35,9 +37,69 @@ class SingleDaySolver:
 
     def solve(self):
         self.solver = pywraplp.Solver.CreateSolver("SAT")
-        self._create_variables()
-        self._create_objective_function()
-        self._create_constraints
+        self._get_solution()
+
+        optimal_lineup, captain = self._get_optimal_lineup()
+        print("The optimal lineup is:")
+        self._print_lineup(optimal_lineup, captain)
+        print(f"With a total score of: {int(self.solver.Objective().Value() + captain.projected_points[self.day - 1])}")
+
+
+    def _get_optimal_lineup(self):
+        indices = list(filter(lambda x: self.solution_values[x], range(self.num_females)))
+        lineup_female = list(map(lambda x: self.female_swimmers[x], indices))
+
+        indices = list(filter(lambda x: self.solution_values[x], range(self.num_females, self.num_females + self.num_males)))
+        lineup_male = list(map(lambda x: self.male_swimmers[x - self.num_females], indices))
+
+        lineup = lineup_female + lineup_male
+        captain = max(lineup, key=lambda x: x.projected_points[self.day - 1])
+
+        return lineup, captain
+
+
+    def _get_solution(self):
+        # Declare decision variables for female swimmers
+        for index in range(self.num_females):
+            new_constraint = f"x{index + 1} = self.solver.IntVar(0, 1, 'x{index + 1}')"
+            exec(new_constraint)
+
+        # Declare decision variables for male swimmers
+        for index in range(self.num_males):
+            new_constraint = f"y{index + 1} = self.solver.IntVar(0, 1, 'y{index + 1}')"
+            exec(new_constraint)
+
+        # Create and declare objective function
+        objective_function = ""
+        for index, swimmer in enumerate(self.female_swimmers):
+            objective_function += f"{swimmer.projected_points[self.day - 1]}*x{index + 1} + "
+        for index, swimmer in enumerate(self.male_swimmers):
+            objective_function += f"{swimmer.projected_points[self.day - 1]}*y{index + 1} + "
+        objective_function = f"self.solver.Maximize({objective_function.rstrip("+ ")})"
+        exec(objective_function)
+    
+        # Budget constraint
+        budget_constraint = ""
+        for index, swimmer in enumerate(self.female_swimmers):
+            budget_constraint += f"{swimmer.cost}*x{index + 1} + "
+        for index, swimmer in enumerate(self.male_swimmers):
+            budget_constraint += f"{swimmer.cost}*y{index + 1} + "
+        budget_constraint = f"self.solver.Add({budget_constraint.rstrip("+ ")} <= {BUDGET})"
+        exec(budget_constraint)
+
+        # Number of females constraint
+        num_females_constraint = "self.solver.Add("
+        for index in range(self.num_females):
+            num_females_constraint += f"x{index + 1} + "
+        num_females_constraint = f"{num_females_constraint.rstrip("+ ")} <= {ROSTER_SIZE / 2})"
+        exec(num_females_constraint)
+
+        # Number of males constraint
+        num_males_constraint = "self.solver.Add("
+        for index in range(self.num_males):
+            num_males_constraint += f"y{index + 1} + "
+        num_males_constraint = f"{num_males_constraint.rstrip("+ ")} <= {ROSTER_SIZE / 2})"
+        exec(num_males_constraint)
 
         # Solve
         print(f"Solving with {self.solver.SolverVersion()}")
@@ -47,109 +109,24 @@ class SingleDaySolver:
         if status != pywraplp.Solver.OPTIMAL:
             raise Exception("No optimal solution found")
 
-        optimal_lineup, captain = self._get_optimal_lineup()
-        print("The optimal lineup is:")
-        self._print_lineup(optimal_lineup, captain)
-        print(f"With a total score of: {self.solver.Objective().Value()}")
-
-
-    def _get_optimal_lineup(self):
-        # Make female_swimmers_list to be executed later"
-        female_swimmers_list = ""
-        for i in range(self.num_females):
-            female_swimmers_list += f"self.x{i + 1}, "
-        female_swimmers_list = f"[{female_swimmers_list.rstrip(", ")}]"
-
-        # Make male_swimmers_list to be executed later"
-        male_swimmers_list = ""
-        for i in range(self.num_males):
-            male_swimmers_list += f"self.y{i + 1}, "
-        male_swimmers_list = f"[{male_swimmers_list.rstrip(", ")}]"
-
-        # Make solution_values_list to be executed later"
+        # Make solution_values_list"
         solution_values_list = ""
         for i in range(self.num_females):
-            solution_values_list += f"self.x{i + 1}.solution_value(), "
+            solution_values_list += f"x{i + 1}.solution_value(), "
         for i in range(self.num_males):
-            solution_values_list += f"self.y{i + 1}.solution_value(), "
+            solution_values_list += f"y{i + 1}.solution_value(), "
         solution_values_list = f"[{solution_values_list.rstrip(", ")}]"
 
-        female_swimmers = []
-        exec("female_swimmers = " + female_swimmers_list)
-        male_swimmers = []
-        exec("male_swimmers = " + male_swimmers_list)
-        solution_values = []
-        exec("solution_values = " + solution_values_list)
-
-        indices = list(filter(lambda x: solution_values[x], range(self.num_females)))
-        lineup_female = list(map(lambda x: self.female_swimmers[x], indices))
-
-        indices = list(filter(lambda x: solution_values[x], range(self.num_females, self.num_females + self.num_males)))
-        lineup_male = list(map(lambda x: self.female_swimmers[x], indices))
-
-        lineup = lineup_female + lineup_male
-        captain = max(lineup, key=lambda x: x.projected_points[self.day - 1])
-
-        return lineup, captain
-
-
-    def _create_variables(self):
-        # declare decision variables for female swimmers
-        for index in range(self.num_females):
-            new_constraint = f"x{index + 1} = self.solver.IntVar(0, 1, 'x{index + 1}')"
-            exec(new_constraint)
-
-        # declare decision variables for male swimmers
-        for index in range(self.num_males):
-            new_constraint = f"y{index + 1} = self.solver.IntVar(0, 1, 'y{index + 1}')"
-            exec(new_constraint)
-    
-
-    def _create_objective_function(self):
-        print(len(self.solver.variables()))
-
-        # create and declare objective function
-        objective_function = ""
-        for index in range(self.num_females):
-            num = self.female_points[index]
-            objective_function += f"{num}*x{index + 1} + "
-        for index in range(self.num_males):
-            num = self.male_points[index]
-            objective_function += f"{num}*y{index + 1} + "
-        objective_function = f"self.solver.Maximize({objective_function.rstrip("+ ")})"
-        exec(objective_function)
-    
-
-    def _create_constraints(self):
-        # budget constraint
-        budget_constraint = ""
-        for index in range(self.num_females):
-            num = self.female_costs[index]
-            budget_constraint += f"{num}*x{index + 1} + "
-        for index in range(self.num_males):
-            num = self.male_costs[index]
-            budget_constraint += f"{num}*y{index + 1} + "
-        budget_constraint = f"self.solver.Add({budget_constraint.rstrip("+ ")} <= {BUDGET})"
-        exec(budget_constraint)
-
-        # number of females constraint
-        num_females_constraint = "self.solver.Add("
-        for index in range(self.num_females):
-            num_females_constraint += f"x{index + 1} + "
-        exec(num_females_constraint.rstrip("+ ") + f" <= {ROSTER_SIZE / 2})")
-
-        # number of males constraint
-        num_males_constraint = "self.solver.Add("
-        for index in range(self.num_males):
-            num_males_constraint += f"y{index + 1} + "
-        exec(num_males_constraint.rstrip("+ ") + f" <= {ROSTER_SIZE / 2})")
+        exec(f"self.solution_values = {solution_values_list}")
     
 
     def _print_lineup(self, lineup, captain):
         for swimmer in lineup:
             print(f"{swimmer.name}", end="")
             if swimmer is captain:
-                print(" (Captain)")
+                print(f" (Captain) ({int(swimmer.projected_points[self.day - 1] * 2)})", end="")
+            else:
+                print(f" ({int(swimmer.projected_points[self.day - 1])})", end="")
             print()
     
 
